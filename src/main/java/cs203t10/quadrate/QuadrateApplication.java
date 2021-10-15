31ed98847ccce9350e0a56ce97a78a5a996a91c2
sin3142 <liu.fangzhou3@gmail.com>
2021-10-15 18:39:04 +0800

    Initial commit

---
+++ src/main/java/cs203t10/quadrate/QuadrateApplication.java
@@ -0,0 +1,13 @@
+package cs203t10.quadrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadrateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs203t10/quadrate/QuadrateApplicationTests.java
@@ -0,0 +1,13 @@
+package cs203t10.quadrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

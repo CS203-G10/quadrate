827ef69b21dca0fd3f2b0c755a939a75f3a0fd12
joylelow <44151277+joylelow@users.noreply.github.com>
2021-10-28 05:47:13 +0800

    CS203-17 CRUD message

---
+++ src/main/java/cs203t10/quadrate/message/MessageService.java
@@ -0,0 +1,58 @@
+package cs203t10.quadrate.message;
+
+import cs203t10.quadrate.exception.MessageNotFoundException;
+import cs203t10.quadrate.exception.UserNotFoundException;
+import cs203t10.quadrate.notification.NotificationService;
+import cs203t10.quadrate.user.UserRepository;
+import cs203t10.quadrate.user.UserService;
+import cs203t10.quadrate.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+    private final NotificationService notificationService;
+
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessage(Long id) throws MessageNotFoundException {
+        return messageRepository.findById(id).orElseThrow(() -> new MessageNotFoundException());
+    }
+
+    public Message createMessage(String username, Message message) throws UserNotFoundException {
+        return userRepository.findByUsername(username).map(user -> {
+            message.setSender(user);
+            message.setUpdater(user);
+            Message savedMessage = messageRepository.save(message);
+            // create notification for target users
+            notificationService.createNotification(savedMessage);
+            return savedMessage;
+        }).orElseThrow(() -> new UserNotFoundException(username));
+    }
+
+    public Message updateMessage(Long id, Message message) throws MessageNotFoundException{
+        Message messageFound = messageRepository.findById(id).orElseThrow(() -> new MessageNotFoundException());
+        messageFound.setSubject(message.getSubject());
+        messageFound.setContent(message.getContent());
+        messageFound.setUpdateDateTime(LocalDateTime.now());
+        // TODO: record updater, need DTO
+
+        return messageRepository.save(messageFound);
+    }
+
+    public Message deleteMessage(Long id) throws MessageNotFoundException{
+        Message messageFound = getMessage(id);
+        messageRepository.deleteById(id);
+        return messageFound;
+    }
+
+}

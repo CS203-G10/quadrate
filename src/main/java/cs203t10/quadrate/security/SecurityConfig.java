ac20fe15ad8aa3bbc3d294a667349e65f272be02
joylelow <44151277+joylelow@users.noreply.github.com>
2021-11-09 19:11:55 +0800

    basic auth

---
+++ src/main/java/cs203t10/quadrate/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package cs203t10.quadrate.security;
+
+import cs203t10.quadrate.user.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true)
+//        securedEnabled = true,
+//        jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+//                //authorization
+                .antMatchers("/api/user", "/api/user/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+//                  .antMatchers("/auth/*").hasAnyRole("ADMIN","USER")
+
+//                .anyRequest()
+//                .authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().disable();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
--- src/main/java/cs203t10/quadrate/user/User.java
+++ src/main/java/cs203t10/quadrate/user/User.java
@@ -16,6 +16,8 @@ import cs203t10.quadrate.interval.Interval;
 // import com.fasterxml.jackson.annotation.JsonManagedReference;
 // import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
@@ -23,7 +25,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(indexes = @Index(columnList = "username", unique = true))
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -84,4 +86,33 @@ public class User {
         this.password = password;
         this.role = role;
     }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String username, String password) {
+        this(username, password, UserRole.USER);
+    }
+
+    public User(String username, String password, UserRole role) {
+        this(username, password, role.getRole());
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return null; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
